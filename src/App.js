c13ff9e6581c6376e5c1d3e7aad15be5510470dc
JCesana <joshuacesana@gmail.com>
2019-02-12 19:46:37 -0800

    Add Home container, fix typo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
-import SideBar from './containers/SideBar/SideBar';
+import {SideBar} from './containers/SideBar/SideBar';
+import {Home} from './containers/Home/Home';
+// import './Home.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <HeaderNav />
-        <SideBar />
-      </div>
+      <React.Fragment>
+        <HeaderNav/>
+        <SideBar/>
+        <Home/>
+      </React.Fragment>
     );
   }
 }
