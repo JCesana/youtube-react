c851b12ddd805267ad2eb549478dc5ce2d2fc436
JCesana <joshuacesana@gmail.com>
2019-02-12 21:39:03 -0800

    Fix import

--- src/containers/SideBar/__tests__/SideBar.unit.test.js
+++ src/containers/SideBar/__tests__/SideBar.unit.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import SideBar from '../SideBar';
+import {SideBar} from '../SideBar';
 
 describe('SideBar', () => {
   test('renders', () => {
